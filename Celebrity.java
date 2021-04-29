fbd0576b521d9f8131e16da60ceadbb000e6deab
knowdawg <33770635+knowdawg@users.noreply.github.com>
2021-04-29 10:40:53 -0700

    Add files via upload

---
+++ Celebrity.java
@@ -0,0 +1,19 @@
+public class Celebrity{
+    private String name;
+    private String clue;
+
+    public Celebrity(String name, String clue){
+        this.name = name;
+        this.clue = clue;
+    }
+
+    public String getName(){
+        return name;
+    }
+    public String getClue(){
+        return clue;
+    }
+    public String toString(){
+        return(name + " : " + clue);
+    }
+}
